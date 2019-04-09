d15bd8f3716c8ac5ac35e70f355408bb3b324ae6
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-04-09 15:01:13 +0200

    fix procsess

--- process.py
+++ process.py
@@ -32,8 +32,7 @@ def process2(img):
         # cv2.waitKey()
 
         ret, thresh = cv2.threshold(img, 127, 255, 0)
-        im2, contours, hierarchy = cv2.findContours(
-            thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         (x, y), radius = cv2.minEnclosingCircle(contours[0])
         x = int(x)
         y = int(y)
